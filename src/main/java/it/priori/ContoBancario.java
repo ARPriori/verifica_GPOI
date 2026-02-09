93097d8aa5741a791f906dd885110d2ad14bf0f4
ARPriori <prioriasia30@gmail.com>
2026-02-09 11:49:32 +0100

    Aggiunta movimenti

--- src/main/java/it/priori/ContoBancario.java
+++ src/main/java/it/priori/ContoBancario.java
@@ -1,5 +1,7 @@
 package it.priori;
 
+import java.util.ArrayList;
+
 /**
  * Classe che rappresenta un conto bancario.
  */
@@ -12,6 +14,10 @@ public class ContoBancario {
      * Il saldo del conto bancario.
      */
     protected double saldo;
+    /**
+     * La lista dei movimenti del conto bancario.
+     */
+    protected ArrayList<String> movimenti = new ArrayList<>();
 
     /**
      * Costruttore che inizializza il conto bancario con un IBAN e un saldo.
@@ -38,9 +44,34 @@ public class ContoBancario {
     public double getSaldo() {
         return saldo;
     }
+    
+    /**
+     * Restituisce la lista dei movimenti del conto bancario.
+     * @return la lista dei movimenti del conto bancario
+     */
+    public ArrayList<String> getMovimenti() {
+        return movimenti;
+    }
 
     /**
-     * Deposita una somma di denaro sul conto bancario, incrementando il saldo.
+     * Restituisce gli ultimi n movimenti del conto bancario.
+     * @param n il numero di movimenti da restituire
+     * @return gli ultimi n movimenti del conto bancario
+     */
+    public ArrayList<String> getUltimiMovimenti(int n){
+        ArrayList<String> ultimiMov = new ArrayList<>();
+        if(n > movimenti.size()){
+            return null;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            ultimiMov.add(movimenti.get(movimenti.size()-i));
+        }
+        return ultimiMov;
+    }
+
+    /**
+     * Deposita una somma di denaro sul conto bancario, incrementando il saldo. Viene aggiornata la lista dei movimenti
      * @param importo la somma di denaro da depositare
      * @throws IllegalArgumentException se l'importo è negativo
      */
@@ -49,11 +80,12 @@ public class ContoBancario {
             throw new IllegalArgumentException();
         }else{
             saldo += importo;
+            movimenti.add("deposito di " + importo + "EUR");
         }
     }
 
     /**
-     * Preleva una somma di denaro dal conto bancario, decrementando il saldo
+     * Preleva una somma di denaro dal conto bancario, decrementando il saldo Viene aggiornata la lista dei movimenti
      * @param importo la somma di denaro da prelevare
      * @throws IllegalArgumentException se l'importo e' negativo o supera il saldo
      */
@@ -62,19 +94,20 @@ public class ContoBancario {
             throw new IllegalArgumentException();
         }else{
             saldo -= importo;
+            movimenti.add("pelievo di " + importo + "EUR");
         }
     }
 
     /**
-     * Restituisce una rappresentazione stringa del conto bancario. Include l'IBAN e il saldo. La rappresentazione stringa e' formattata per essere facile da leggere e comprendere. Ecco un esempio di come potrebbe apparire la rappresentazione stringa:
+     * Restituisce una rappresentazione stringa del conto bancario. Include l'IBAN, il saldo e l'ultimo movimento effettuato (se presenti). La rappresentazione stringa e' formattata per essere facile da leggere e comprendere. Ecco un esempio di come potrebbe apparire la rappresentazione stringa:
      * <br>```
-     * <br>Conto Bancario:<br>IBAN: IT12X0542811101000000123<br>Saldo: 1000.0 EUR
+     * <br>Conto Bancario:<br>IBAN: IT12X0542811101000000123<br>Saldo: 1000.0 EUR<br>Ultimo Movimento:<br>- deposito di 100 EUR
      * <br>```
-     * @return una rappresentazione stringa del conto bancario, inclusivo di IBAN e saldo
+     * @return una rappresentazione stringa del conto bancario, inclusivo di IBAN, saldo e ultimo movimento
      */
     @Override
     public String toString() {
-        return "Conto Bancario:\nIBAN: " + iban + "\nSaldo: " + saldo + "EUR";
+        return "Conto Bancario:\nIBAN: " + iban + "\nSaldo: " + saldo + "EUR\nUltimo Movimento:\n" + ((movimenti.size() == 0)? "nessun movimento registrato" : movimenti.getLast());
     }
     
 }
