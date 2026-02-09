744eaf2745453eb5ad6d618d7b4f93a096b16480
ARPriori <prioriasia30@gmail.com>
2026-02-09 11:25:26 +0100

    Riempimento main

--- src/main/java/it/priori/Main.java
+++ src/main/java/it/priori/Main.java
@@ -2,6 +2,13 @@ package it.priori;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ContoBancario cb = new ContoBancario("IT12X0542811101000000123", 1000);
+        System.out.println(cb.toString());
+
+        cb.preleva(100);
+        System.out.println(cb.toString());
+
+        cb.deposita(1000);
+        System.out.println(cb.toString());
     }
 }
